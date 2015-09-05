d7043b254df3c15c8898fb0170eb49e666950226
Aaron Williams <aaron.colby.williams@gmail.com>
2015-09-04 17:12:58 -0700

    initial swing at design comps

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,6 +4,35 @@ var App = App || {};
 
 App = {
   init: function () {
-    console.log('The Voice of the West. The Voice of San Francisco and the Bay Area.');
+      console.log('Wdaup');
+      this.nav();
+      this.profileList();
+  },
+  nav: function () {
+      (function() {
+        var body = document.body;
+        var burgerMenu = document.getElementsByClassName('b-menu')[0];
+        var burgerContain = document.getElementsByClassName('b-container')[0];
+        var burgerNav = document.getElementsByClassName('b-nav')[0];
+
+        burgerMenu.addEventListener('click', function toggleClasses() {
+          [body, burgerContain, burgerNav].forEach(function (el) {
+            el.classList.toggle('open');
+          });
+        }, false);
+      })();
+  },
+  profileList: function () {
+      var $characters = $('.character-selection');
+      setBg( $characters.parent().first().children() );
+
+      $characters.on('mouseenter', function (event) {
+          setBg(this);
+      });
+
+      function setBg (node) {
+          var imageUrl = $(node).data('url');
+          $('.img-container').css('background-image', 'url('+imageUrl+')');
+      }
   }
 };
--- views.py
+++ views.py
@@ -7,3 +7,11 @@ from models import *
 @app.route('/')
 def index():
     return render_template('index.html', title='index')
+
+@app.route('/p/')
+def profiles():
+    return render_template('profiles/index.html', title='profiles')
+
+@app.route('/p/<profile>')
+def profile(profile):
+    return render_template('profiles/{}.html'.format(profile), title='profile')
