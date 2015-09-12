6a8e024e4bb7a8bb904c071276e1aadb6c2cdd1d
Aaron Williams <aaron.colby.williams@gmail.com>
2015-09-11 17:33:53 -0700

    added asset pipeline, dropcap, new menu and broken model.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,35 +4,38 @@ var App = App || {};
 
 App = {
   init: function () {
-      console.log('Wdaup');
-      this.nav();
-      this.profileList();
+    this.nav();
+    this.profileList();
   },
   nav: function () {
-      (function() {
-        var body = document.body;
-        var burgerMenu = document.getElementsByClassName('b-menu')[0];
-        var burgerContain = document.getElementsByClassName('b-container')[0];
-        var burgerNav = document.getElementsByClassName('b-nav')[0];
+    (function() {
+      var body = document.body;
+      var burgerMenu = document.getElementsByClassName('b-menu')[0];
+      var burgerContain = document.getElementsByClassName('b-container')[0];
+      var burgerNav = document.getElementsByClassName('b-nav')[0];
 
-        burgerMenu.addEventListener('click', function toggleClasses() {
-          [body, burgerContain, burgerNav].forEach(function (el) {
-            el.classList.toggle('open');
-          });
-        }, false);
-      })();
+      burgerMenu.addEventListener('click', function toggleClasses() {
+        [body, burgerContain, burgerNav].forEach(function (el) {
+          el.classList.toggle('open');
+        });
+      }, false);
+    })();
   },
   profileList: function () {
-      var $characters = $('.character-selection');
-      setBg( $characters.parent().first().children() );
+    var $characters = $('.character-selection');
+    $characters.on('mouseenter', function (event) { setBg(this); })
 
-      $characters.on('mouseenter', function (event) {
-          setBg(this);
-      });
-
-      function setBg (node) {
-          var imageUrl = $(node).data('url');
-          $('.img-container').css('background-image', 'url('+imageUrl+')');
-      }
+    function setBg (node) {
+      var imageUrl = $(node).data('url');
+      $('.img-container').css('background-image', 'url('+imageUrl+')');
+    }
+  },
+  enableDropcap: function () {
+    /* EXPERIMENT */
+    var dropcap = document.getElementById('dropcap');
+    window.Dropcap.layout(dropcap, 8);
+  },
+  enableVideoModal: function () {
+    $('.video').animatedModal();
   }
 };
--- views.py
+++ views.py
@@ -3,10 +3,10 @@ from flask import render_template  #, redirect, url_for
 from app import app, db
 from models import Profile
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', title='index')
+    profiles = Profile.query.all()
+    return render_template('index.html', title='index', profiles=profiles)
 
 @app.route('/p/')
 def profiles():
@@ -19,5 +19,7 @@ def profiles():
 
 @app.route('/p/<slug>/')
 def profile(slug):
+    profiles = Profile.query.all()
     profile = Profile.query.filter_by(slug=slug).all()[0]
-    return render_template('profiles/profile.html', title='profile', profile=profile)
+    return render_template('profiles/profile.html',
+        title='profile', profile=profile, profiles=profiles)
