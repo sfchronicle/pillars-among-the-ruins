ef947d184acf1006bfad151f5dab9485c8ee747b
Aaron Williams <aaron.colby.williams@gmail.com>
2015-10-08 15:54:54 -0700

    type adjustment and panel implementation

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,6 +4,23 @@ var App = App || {};
 
 App = {
   init: function () {
-    console.log('Pillars Among the Ruins');
+    this.dropcap();
+    this.triggerViz();
+  },
+  dropcap: function () {
+    var dropcaps = document.querySelectorAll('.dropcap');
+    window.Dropcap.layout(dropcaps, 3);
+  },
+  triggerViz: function () {
+    $('aside#one').scotchPanel({
+      containerSelector: '.viz',
+      direction: 'left',
+      duration: 300,
+      transition: 'ease',
+      clickSelector: '.viz-trigger',
+      distanceX: '100%',
+      forceMinHeight: true,
+      enableEscapeKey: true
+    });
   }
 };
