9c3e8029ebbd48112b8a6f5a93e842e986fb0485
Aaron Williams <aaron.colby.williams@gmail.com>
2015-10-13 17:49:29 -0700

    minor nav implementation

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -6,6 +6,7 @@ App = {
   init: function () {
     this.dropcap();
     this.fadeStoryIn();
+    this.nav();
   },
   dropcap: function () {
     var dropcaps = document.querySelectorAll('.dropcap');
@@ -27,5 +28,62 @@ App = {
     });
   },
   nav: function () {
+    //open navigation clicking the menu icon
+  	$('.nav-trigger').on('click', function(event){
+  		event.preventDefault();
+  		toggleNav(true);
+  	});
+  	//close the navigation
+  	$('.cd-close-nav, .overlay').on('click', function(event){
+  		event.preventDefault();
+  		toggleNav(false);
+  	});
+  	//select a new section
+  	// $('.nav-list li').on('click', function(event){
+  	// 	event.preventDefault();
+  	// 	var target = $(this),
+  	// 		//detect which section user has chosen
+  	// 		sectionTarget = target.data('menu');
+  	// 	if( !target.hasClass('cd-selected') ) {
+  	// 		//if user has selected a section different from the one alredy visible
+  	// 		//update the navigation -> assign the .cd-selected class to the selected item
+  	// 		target.addClass('cd-selected').siblings('.cd-selected').removeClass('cd-selected');
+  	// 		//load the new section
+  	// 		loadNewContent(sectionTarget);
+  	// 	} else {
+  	// 		// otherwise close navigation
+  	// 		toggleNav(false);
+  	// 	}
+  	// });
+
+  	function toggleNav(bool) {
+  		$('nav, .overlay').toggleClass('is-visible', bool);
+  		$('main').toggleClass('scale-down', bool);
+  	}
+
+  	function loadNewContent(newSection) {
+  		//create a new section element and insert it into the DOM
+  		var section = $('<section class="cd-section '+newSection+'"></section>').appendTo($('main'));
+  		//load the new content from the proper html file
+  		section.load(newSection+'.html .cd-section > *', function(){
+  			//add the .cd-selected to the new section element -> it will cover the old one
+  			section.addClass('cd-selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+  				//close navigation
+  				toggleNav(false);
+  			});
+  			section.prev('.cd-selected').removeClass('cd-selected');
+  		});
+
+  		$('main').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+  			//once the navigation is closed, remove the old section from the DOM
+  			section.prev('.cd-section').remove();
+  		});
+
+  		if( $('.no-csstransitions').length > 0 ) {
+  			//if browser doesn't support transitions - don't wait but close navigation and remove old item
+  			toggleNav(false);
+  			section.prev('.cd-section').remove();
+  		}
+  	}
   }
 };
