b3b100d28a0f608d6ea32ceb2c1385013cd0d4e2
Aaron Williams <aaron.colby.williams@gmail.com>
2015-10-28 13:51:42 -0700

    Added Slide to admin interface

--- admin.py
+++ admin.py
@@ -2,8 +2,9 @@ import flask_admin as admin
 from flask_admin.contrib.sqla import ModelView
 
 from app import app, db
-from models import Profile
+from models import Profile, Slide
 
 # Admin
 admin = admin.Admin(app)
 admin.add_view(ModelView(Profile, db.session))
+admin.add_view(ModelView(Slide, db.session))
